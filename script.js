e36b3f041cfc0a9805d7f7c45f70ce7d9db98232
Ammymay <ammymay2003@gmail.com>
2025-05-11 17:38:41 +0100

    modified files

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function greetuser(Alice) {
-    console.log("Hello Alice! Welcome to JavaScript");
+function greetUser(name, lastmessage) {
+    console.log('Hello'+ name + '' + lastmessage);
 }
 
+greetUser('Alice!', 'Welcome');
+
+greetUser('Chiamaka!', 'Welcome to CSS');
+
+greetUser('Coach Daniel', 'Welcome to HTML');
